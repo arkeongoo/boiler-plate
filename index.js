b7e6667245e8df9d5b41d6400ff275e484ced61b
arkeongoo <arkeongoo@naver.com>
2025-03-26 21:54:05 +0900

    secret info config

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+// const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+const config = require("./config/key");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://arkeongoo:mqkrdlgus1!@boilerplate.xx4inus.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    return res.status(400).json({ success: false, error: err.message });
+  }
 });
 
 app.listen(port, () => {
